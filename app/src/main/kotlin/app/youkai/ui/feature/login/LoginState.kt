cc960509f60c72439967b60e75bfc0a2016d720d
Ihsan Isik <x.ihsan@gmail.com>
2017-02-20 02:37:45 +0100

    Remove unnecessary state items

--- app/src/main/kotlin/app/youkai/ui/feature/login/LoginState.kt
+++ app/src/main/kotlin/app/youkai/ui/feature/login/LoginState.kt
@@ -5,8 +5,6 @@ import com.hannesdorfmann.mosby.mvp.viewstate.RestorableViewState
 
 class LoginState : RestorableViewState<LoginView> {
     var error: String? = null
-    var usernameRequired = false
-    var passwordRequired = false
     var usernameEnabled = true
     var passwordEnabled = true
     var buttonEnabled = true
@@ -16,8 +14,8 @@ class LoginState : RestorableViewState<LoginView> {
         if (error != null) {
             view.showError(error!!)
         }
-        view.showUsernameRequired(usernameRequired)
-        view.showPasswordRequired(passwordRequired)
+        view.enableUsername(usernameEnabled)
+        view.enablePassword(passwordEnabled)
         view.enableButton(buttonEnabled)
         view.showProgress(progressVisible)
     }
@@ -25,8 +23,6 @@ class LoginState : RestorableViewState<LoginView> {
     override fun restoreInstanceState(bundle: Bundle?): RestorableViewState<LoginView> {
         val state = LoginState()
         state.error = bundle?.getString(KEY_ERROR)
-        state.usernameRequired = bundle?.getBoolean(KEY_USERNAME_REQUIRED) ?: false
-        state.passwordRequired = bundle?.getBoolean(KEY_PASSWORD_REQUIRED) ?: false
         state.usernameEnabled = bundle?.getBoolean(KEY_USERNAME_ENABLED) ?: false
         state.passwordEnabled = bundle?.getBoolean(KEY_PASSWORD_ENABLED) ?: false
         state.buttonEnabled = bundle?.getBoolean(KEY_BUTTON_ENABLED) ?: false
@@ -36,8 +32,6 @@ class LoginState : RestorableViewState<LoginView> {
 
     override fun saveInstanceState(bundle: Bundle) {
         bundle.putString(KEY_ERROR, error)
-        bundle.putBoolean(KEY_USERNAME_REQUIRED, usernameRequired)
-        bundle.putBoolean(KEY_PASSWORD_REQUIRED, passwordRequired)
         bundle.putBoolean(KEY_BUTTON_ENABLED, usernameEnabled)
         bundle.putBoolean(KEY_BUTTON_ENABLED, passwordEnabled)
         bundle.putBoolean(KEY_BUTTON_ENABLED, buttonEnabled)
@@ -46,8 +40,6 @@ class LoginState : RestorableViewState<LoginView> {
 
     companion object {
         private val KEY_ERROR = "error"
-        private val KEY_USERNAME_REQUIRED = "username_required"
-        private val KEY_PASSWORD_REQUIRED = "password_required"
         private val KEY_USERNAME_ENABLED = "username_enabled"
         private val KEY_PASSWORD_ENABLED = "password_enabled"
         private val KEY_BUTTON_ENABLED = "button_enabled"
