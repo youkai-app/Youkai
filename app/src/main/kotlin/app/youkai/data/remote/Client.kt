787290e277454c04386f3912cf9ddf8869d4d9a4
dulleh <dulleftovers@gmail.com>
2017-02-25 00:50:23 +0000

    Documentation for Client.kt

--- app/src/main/kotlin/app/youkai/data/remote/Client.kt
+++ app/src/main/kotlin/app/youkai/data/remote/Client.kt
@@ -2,7 +2,10 @@ package app.youkai.data.remote
 
 import okhttp3.OkHttpClient
 
-
+/*
+ * Client to be used for Kitsu api calls only. Would need a separate client or else make
+ * minor modifications to JsonHeadersInterceptor.kt to allow for non-json responses.
+ */
 val Client by lazy {
     OkHttpClient()
             .newBuilder()
