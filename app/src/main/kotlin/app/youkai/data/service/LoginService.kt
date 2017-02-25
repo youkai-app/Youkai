eef894ec2c060d63f9177baddcb711575548f705
dulleh <dulleftovers@gmail.com>
2017-02-25 01:37:09 +0000

    Separate services for standard API calls and login

---
+++ app/src/main/kotlin/app/youkai/data/service/LoginService.kt
@@ -0,0 +1,17 @@
+package app.youkai.data.service
+
+import app.youkai.data.models.Credentials
+import io.reactivex.Observable
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface LoginService {
+
+    @POST("oauth/token")
+    @Headers("Content-Type: application/x-www-form-urlencoded")
+    @FormUrlEncoded
+    fun  login(@Field("username") username: String, @Field("password") password: String, @Field("grant_type") grantType: String) : Observable<Credentials>
+
+}
