af430c1383448fa6400aefac9b79301d26b2d309
dulleh <dulleftovers@gmail.com>
2017-02-25 00:56:41 +0000

    Initial login API
    BASE URL for Kitsu changed because oauth doesn't follow on from the
    /edge endpoint. The api for oauth also does not give a response in
    JSONAPI format so a standard json convertor factory is needed for
    KitsuApi.login() calls. The oauth endpoint also requires a different
    Content-Type so a check had to be included in JsonHeaderInterceptor to
    prevent it being overrriden (test included).

--- app/src/test/kotlin/app/youkai/NetworkTests.kt
+++ app/src/test/kotlin/app/youkai/NetworkTests.kt
@@ -3,9 +3,12 @@ package app.youkai
 import app.youkai.data.remote.Client
 import okhttp3.Request
 import org.junit.Test
+import java.util.logging.Logger
 import kotlin.test.assertEquals
 
 class NetworkTests {
+
+    val logger = Logger.getAnonymousLogger()
 
     /*
      * Checks that the interceptors in Client.kt are correctly adding the required headers to requests.
@@ -13,12 +16,25 @@ class NetworkTests {
     @Test
     @Throws(Exception::class)
     fun networkInterceptorsTest () {
-        val response = Client.newCall(
+        var response = Client.newCall(
                 Request.Builder()
                 .url("http://xiprox.me")
                 .addHeader("not_a_header", "---").build()).execute()
 
         assertEquals("application/vnd.api+json", response.request().header("Accept"))
+        assertEquals("application/vnd.api+json", response.request().header("Content-Type"))
+
+        response = Client.newCall(
+                Request.Builder()
+                        .url("http://xiprox.me")
+                        .addHeader("not_a_header", "---")
+                        .addHeader("Accept", "Don'TACCEPTS").build()).execute()
+
+        /*
+         * If the Accept header is already present, don't override it.
+         * Always override response for requests to Kitsu.
+         */
+        assertEquals("Don'TACCEPTS", response.request().header("Accept"))
         assertEquals("application/vnd.api+json", response.request().header("Content-Type"))
     }
 
