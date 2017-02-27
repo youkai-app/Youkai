2acc73c2777b93d31fea09ede030c509ab8fb0c0
dulleh <dulleftovers@gmail.com>
2017-02-27 00:14:37 +0000

    Casting model, Relationships WIP
    Basic casting model is complete, the integration with Anime is not.
    Renaming of sample data for unit tests. BaseJsonModel:id private val ->
    var

---
+++ app/src/main/kotlin/app/youkai/data/models/Meta.kt
@@ -0,0 +1,7 @@
+package app.youkai.data.models
+
+class Meta {
+
+    var type: String? = null;
+
+}
--- app/src/test/kotlin/app/youkai/JsonParsingTests.kt
+++ app/src/test/kotlin/app/youkai/JsonParsingTests.kt
@@ -1,7 +1,6 @@
 package app.youkai
 
-import app.youkai.data.models.Anime
-import app.youkai.data.models.Credentials
+import app.youkai.data.models.*
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.jasminb.jsonapi.ResourceConverter
 import org.junit.Test
@@ -16,48 +15,50 @@ class JsonParsingTests {
     @Test
     @Throws(Exception::class)
     fun animeTest1 () {
-
         val resourceConverter = ResourceConverter(Anime::class.java)
 
-        val testJson = ClassLoader.getSystemClassLoader().getResourceAsStream("anime_json_response")
+        val testJson = ClassLoader.getSystemClassLoader().getResourceAsStream("anime_sparse_json")
         val animeJsonDoc = resourceConverter.readDocument(testJson, Anime::class.java)
         val anime = animeJsonDoc.get()
+
+        assertEquals("7442", anime.id)
+        //assertEquals("anime", animeJsonDoc.meta["type"])
+        //assertEquals("https://kitsu.io/api/edge/anime/7442", animeJsonDoc.links.getLink("self"))
 
         assertEquals("attack-on-titan", anime.slug)
         assertEquals("Centuries ago, manki...defeating them before the last walls are breached.\r\n[Written by MAL Rewrite]", anime.synopsis)
         assertEquals(263, anime.coverImageTopOffset)
-        assertEquals("Attack on Titan", anime.titles?.en)
-        assertEquals("Shingeki no Kyojin", anime.titles?.enJp)
-        assertEquals("進撃の巨人", anime.titles?.jaJp)
+        assertEquals("Attack on Titan", anime.titles!!.en)
+        assertEquals("Shingeki no Kyojin", anime.titles!!.enJp)
+        assertEquals("進撃の巨人", anime.titles!!.jaJp)
         assertEquals("Shingeki no Kyojin", anime.canonicalTitle)
-        assertEquals(4.2380021637554, anime.averageRating)
+        assertEquals(4.23691079690624, anime.averageRating)
         anime.abbreviatedTitles?.forEach { title -> assertEquals("AoT", title) }
-        assertEquals("141", anime.ratingFrequencies?.get("0.5")) //Don't check all because long.
-        assertEquals("2521", anime.ratingFrequencies?.get("3.0"))
-        assertEquals("13279", anime.ratingFrequencies?.get("5.0"))
-        assertEquals(4017, anime.favoritesCount)
+        assertEquals("141", anime.ratingFrequencies!!.get("0.5")) //Don't check all because long.
+        assertEquals("2526", anime.ratingFrequencies!!.get("3.0"))
+        assertEquals("13299", anime.ratingFrequencies!!.get("5.0"))
+        assertEquals(4024, anime.favoritesCount)
         assertEquals("2013-04-07", anime.startDate)
         assertEquals("2013-09-29", anime.endDate)
         assertEquals(2, anime.popularityRank)
-        assertEquals(131, anime.ratingRank)
+        assertEquals(132, anime.ratingRank)
         assertEquals("R", anime.ageRating)
         assertEquals("17+ (violence & profanity)", anime.ageRatingGuide)
-        assertEquals("https://media.kitsu.io/anime/poster_images/7442/tiny.jpg?1418580054", anime.posterImage?.tiny)
-        assertEquals("https://media.kitsu.io/anime/poster_images/7442/small.jpg?1418580054", anime.posterImage?.small)
-        assertEquals("https://media.kitsu.io/anime/poster_images/7442/medium.jpg?1418580054", anime.posterImage?.medium)
-        assertEquals("https://media.kitsu.io/anime/poster_images/7442/large.jpg?1418580054", anime.posterImage?.large)
-        assertEquals("https://media.kitsu.io/anime/poster_images/7442/original.jpg?1418580054", anime.posterImage?.original)
-        assertEquals("https://media.kitsu.io/anime/cover_images/7442/tiny.jpg?1486065327", anime.coverImage?.tiny)
-        assertEquals("https://media.kitsu.io/anime/cover_images/7442/small.jpg?1486065327", anime.coverImage?.small)
-        assertEquals("https://media.kitsu.io/anime/cover_images/7442/large.jpg?1486065327", anime.coverImage?.large)
-        assertEquals("https://media.kitsu.io/anime/cover_images/7442/original.png?1486065327", anime.coverImage?.original)
+        assertEquals("https://media.kitsu.io/anime/poster_images/7442/tiny.jpg?1418580054", anime.posterImage!!.tiny)
+        assertEquals("https://media.kitsu.io/anime/poster_images/7442/small.jpg?1418580054", anime.posterImage!!.small)
+        assertEquals("https://media.kitsu.io/anime/poster_images/7442/medium.jpg?1418580054", anime.posterImage!!.medium)
+        assertEquals("https://media.kitsu.io/anime/poster_images/7442/large.jpg?1418580054", anime.posterImage!!.large)
+        assertEquals("https://media.kitsu.io/anime/poster_images/7442/original.jpg?1418580054", anime.posterImage!!.original)
+        assertEquals("https://media.kitsu.io/anime/cover_images/7442/tiny.jpg?1486065327", anime.coverImage!!.tiny)
+        assertEquals("https://media.kitsu.io/anime/cover_images/7442/small.jpg?1486065327", anime.coverImage!!.small)
+        assertEquals("https://media.kitsu.io/anime/cover_images/7442/large.jpg?1486065327", anime.coverImage!!.large)
+        assertEquals("https://media.kitsu.io/anime/cover_images/7442/original.png?1486065327", anime.coverImage!!.original)
         assertEquals(25, anime.episodeCount)
         assertEquals(24, anime.episodeLength)
         assertEquals("TV" , anime.subtype)
         assertEquals("LHtdKWJdif4", anime.youtubeVideoId)
         assertEquals("TV", anime.showType)
         assertEquals(false, anime.nsfw)
-
     }
 
     @Test
@@ -75,5 +76,39 @@ class JsonParsingTests {
         assertEquals("1486761182", credentials.createdAt)
 
     }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun castingsTest () {
+        val resourceConverter = ResourceConverter(Castings::class.java)
+
+        val testJson = ClassLoader.getSystemClassLoader().getResourceAsStream("castings_json")
+        val castingsJsonDoc = resourceConverter.readDocumentCollection(testJson, Castings::class.java)
+        val castings = castingsJsonDoc.get()
+
+        assertEquals("121206", castings[0].id)
+        assertEquals("Director", castings[0]!!.role)
+        assertEquals(false, castings[0]!!.isVoiceActor)
+        assertEquals(false, castings[0]!!.featured)
+        assertEquals(null, castings[0]!!.language)
+        assertEquals(64, castingsJsonDoc.meta["count"])
+        assertEquals("https://kitsu.io/api/edge/anime/7442/castings?page%5Blimit%5D=10&page%5Boffset%5D=10", castingsJsonDoc.links.links["next"]!!.href)
+        assertEquals("https://kitsu.io/api/edge/anime/7442/castings?page%5Blimit%5D=10&page%5Boffset%5D=54", castingsJsonDoc.links.links["last"]!!.href)
+        assertEquals("https://kitsu.io/api/edge/anime/7442/castings?page%5Blimit%5D=10&page%5Boffset%5D=0", castingsJsonDoc.links.links["first"]!!.href)
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun animeWithCastingsTest () {
+        val resourceConverter = ResourceConverter(Anime::class.java)
+
+        val testJson = ClassLoader.getSystemClassLoader().getResourceAsStream("anime_with_castings_json")
+        val animeJsonDoc = resourceConverter.readDocument(testJson, Anime::class.java)
+        val anime = animeJsonDoc.get()
+
+    }
+
 
 }
