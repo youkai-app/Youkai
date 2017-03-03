31e389f9026dcb4e1f6d756bbcaa860f99e31567
dulleh <dulleftovers@gmail.com>
2017-03-03 23:45:57 +0000

    Episodes

---
+++ app/src/main/kotlin/app/youkai/data/models/Episode.kt
@@ -0,0 +1,50 @@
+package app.youkai.data.models
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.github.jasminb.jsonapi.annotations.*
+import com.github.jasminb.jsonapi.annotations.Meta
+
+@Type("episodes")
+class Episode : BaseJsonModel() {
+
+    var titles: Titles? = null
+
+    var canonicalTitle: String? = null
+
+    var seasonNumber: Int? = null
+
+    var number: Int? = null
+
+    var synopsis: String? = null
+
+    var airdate: String? = null
+
+    var length: Int? = null
+
+    /*
+     * Thumnail is an object with a single field ("original") in the api.
+     * This method prevents the need for creating a new object for this one property.
+     * If the thumbnail object one day includes new properties (pretty much guaranteed to be strings)
+     * this method should still work as expected.
+     */
+    var thumbnail: String? = null
+
+    @JsonProperty("thumbnail")
+    fun setThumbnail(m: Map<String, String>) {
+        thumbnail = m["original"];
+    }
+
+    @Relationship("media")
+    var anime: Anime? = null
+
+    @RelationshipLinks("media")
+    var animeLinks: com.github.jasminb.jsonapi.Links? = null
+
+    @Meta
+    var meta: CountMeta? = null
+
+    @Links
+    var links: com.github.jasminb.jsonapi.Links? = null
+
+}
+
