128dddb6e43ffddbb976643e26512f5425aec86c
İhsan Işık <x.ihsan@gmail.com>
2017-03-07 20:14:42 +0100

    Initial MediaActivity

---
+++ app/src/main/kotlin/app/youkai/ui/feature/media/MediaActivity.kt
@@ -0,0 +1,11 @@
+package app.youkai.ui.feature.media
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MediaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
