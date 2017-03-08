d81d33d7565eb952037a550e93c85396662b9398
dulleh <dulleftovers@gmail.com>
2017-03-08 16:25:34 +0000

    Media class
    Anime, Manga (and soon Drama) extend from the base class. Only fields
    included in both Anime and Manga currently have been included in the
    Media model.

--- app/src/main/kotlin/app/youkai/data/models/Anime.kt
+++ app/src/main/kotlin/app/youkai/data/models/Anime.kt
@@ -1,52 +1,13 @@
 package app.youkai.data.models
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.github.jasminb.jsonapi.Links
 import com.github.jasminb.jsonapi.annotations.Relationship
 import com.github.jasminb.jsonapi.annotations.RelationshipLinks
 import com.github.jasminb.jsonapi.annotations.Type
 
-@Type("anime") @JsonIgnoreProperties(ignoreUnknown = true) //TODO: REMOVE @JsonIgnoreProperties
-class Anime : BaseJsonModel() {
-
-    var slug: String? = null
-
-    var synopsis: String? = null
-
-    var coverImageTopOffset: Int? = null
-
-    var titles: Titles? = null
-
-    var canonicalTitle: String? = null
-
-    var abbreviatedTitles: List<String>? = null
-
-    var averageRating: Double? = null
-
-    var ratingFrequencies: Map<String, String>? = null
-
-    var favoritesCount: Int? = null
-
-    var startDate: String? = null
-
-    var endDate: String? = null
-
-    var popularityRank: Int? = null
-
-    var ratingRank: Int? = null
-
-    var ageRating: String? = null
-
-    var ageRatingGuide: String? = null
-
-    var posterImage: PosterImage? = null
-
-    var coverImage: CoverImage? = null
-
-    var episodeCount: Int? = null
-
-    var subtype: String? = null
+@Type("anime")
+class Anime : Media() {
 
     var episodeLength: Int? = null
 
@@ -57,29 +18,11 @@ class Anime : BaseJsonModel() {
     @JsonProperty("nsfw")
     var isNsfw: Boolean? = null
 
-    @Relationship("genres")
-    var genres: List<Genre>? = null
-
-    @RelationshipLinks("genres")
-    var genreLinks: Links? = null
-
-    @Relationship("castings")
-    var castings: List<Casting>? = null
-
-    @RelationshipLinks("castings")
-    var castingLinks: Links? = null
-
     @Relationship("episodes")
     var episodes: List<Episode>? = null
 
     @RelationshipLinks("episodes")
     var episodeLinks: Links? = null
-
-    @Relationship("mappings")
-    var mappings: List<Mapping>? = null
-
-    @RelationshipLinks("mappings")
-    var mappingLinks: Links? = null
 
     @Relationship("animeProductions")
     var productions: List<AnimeProduction>? = null
@@ -98,13 +41,5 @@ class Anime : BaseJsonModel() {
 
     @RelationshipLinks("animeStaff")
     var animeStaffLinks: Links? = null
-
-    //TODO: relationships: reviews, mediaRelationships
-
-    @Relationship("installments")
-    var installments: List<Installment>? = null
-
-    @RelationshipLinks("installments")
-    var installmentLinks: Links? = null
 
 }
