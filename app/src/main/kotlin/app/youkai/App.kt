0485bee75ac4088d5d2846dc835da5367e2eed6e
İhsan Işık <x.ihsan@gmail.com>
2017-03-14 14:40:19 +0100

    Depend on and setup Fresco for image loading

--- app/src/main/kotlin/app/youkai/App.kt
+++ app/src/main/kotlin/app/youkai/App.kt
@@ -2,12 +2,14 @@ package app.youkai
 
 import android.app.Application
 import android.content.Context
+import com.facebook.drawee.backends.pipeline.Fresco
 
 class App : Application() {
 
     override fun onCreate() {
         context = this
         super.onCreate()
+        Fresco.initialize(this)
     }
 
     companion object {
