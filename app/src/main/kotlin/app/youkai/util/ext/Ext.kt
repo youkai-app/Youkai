b60235cc41d9bd4c402093064a085a32519552cd
dulleh <dulleftovers@gmail.com>
2017-03-25 17:17:09 +0000

    State changes handled.

--- app/src/main/kotlin/app/youkai/util/ext/Ext.kt
+++ app/src/main/kotlin/app/youkai/util/ext/Ext.kt
@@ -1,5 +1,7 @@
 package app.youkai.util.ext
 
+import android.widget.EditText
+
 /**
  * Various extension functions for various purposes that don't really belong in a specific file.
  */
@@ -11,11 +13,23 @@ fun Any.unitify() {
     /* do nothing */
 }
 
+/**
+ * Appends a string to a string.
+ */
 fun String.append(other: String, delimiter: String = ""): String {
     return if (!isEmpty()) plus(delimiter).plus(other) else other
 }
 
+/**
+ * Appends a string to an entry in a MutableMap.
+ */
 fun MutableMap<String, String>.append(key: String, other: String, delimiter: String = ""): MutableMap<String, String> {
     put(key, (this[key] ?: "").append(other, delimiter))
     return this
 }
+
+/**
+ * Returns the input as a string.
+ */
+fun EditText.inputString(): String = text.toString()
+
