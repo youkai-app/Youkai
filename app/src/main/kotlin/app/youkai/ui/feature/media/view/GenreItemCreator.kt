987c9b8e82b3ad150de3ef13d30e8ef445a5f0f1
İhsan Işık <x.ihsan@gmail.com>
2017-03-31 16:54:04 +0200

    Update genre items with some visual changes and real type

--- app/src/main/kotlin/app/youkai/ui/feature/media/view/GenreItemCreator.kt
+++ app/src/main/kotlin/app/youkai/ui/feature/media/view/GenreItemCreator.kt
@@ -1,9 +1,10 @@
-package app.youkai.ui.common.view
+package app.youkai.ui.feature.media.view
 
 import android.view.View
 import android.widget.TextView
 import app.youkai.App
 import app.youkai.R
+import app.youkai.data.models.Genre
 import app.youkai.util.ext.getLayoutInflater
 
 /**
@@ -13,10 +14,11 @@ object GenreItemCreator {
 
     /**
      * Inflates a new genre item, sets its text to a given string, and returns the resulting view.
+     *
      */
-    fun new(genre: String) : View {
+    fun new(genre: Genre): View {
         val view = App.context.getLayoutInflater().inflate(R.layout.item_genre, null, false)
-        (view as TextView).text = genre
+        (view as TextView).text = genre.name
         return view
     }
 }
