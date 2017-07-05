5056dddc6d7e7399944e4a30d668bc1877510361
İhsan Işık <x.ihsan@gmail.com>
2017-07-05 13:52:15 +0400

    Don't publish to Play if not the master branch

--- scripts/play-publish-if-not-pr.sh
+++ scripts/play-publish-if-not-pr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -ev
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ $TRAVIS_BRANCH == 'master' ]; then
   echo "Assembling and publishing release"
   ./gradlew assembleRelease publishApkRelease
 else
