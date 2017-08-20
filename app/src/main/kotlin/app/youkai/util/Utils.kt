b629504c9a2e70b6599f0d13224d3c0f35a70d49
İhsan Işık <x.ihsan@gmail.com>
2017-08-20 16:09:14 +0400

    Add util function for colored SpannableString generation

--- app/src/main/kotlin/app/youkai/util/Utils.kt
+++ app/src/main/kotlin/app/youkai/util/Utils.kt
@@ -1,6 +1,9 @@
 package app.youkai.util
 
 import android.support.annotation.IntegerRes
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import app.youkai.App
 
 /**
@@ -12,3 +15,12 @@ fun color(@IntegerRes res: Int) = App.context.resources.getColor(res)
  * Convenience function to get strings from anywhere
  */
 fun string(@IntegerRes res: Int): String = App.context.resources.getString(res)
+
+/**
+ * Colors a given span in a string and returns a SpannableString
+ */
+fun coloredSpannableString(text: String, color: Int, start: Int, end: Int): SpannableString {
+    val spannable = SpannableString(text)
+    spannable.setSpan(ForegroundColorSpan(color), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    return spannable
+}
