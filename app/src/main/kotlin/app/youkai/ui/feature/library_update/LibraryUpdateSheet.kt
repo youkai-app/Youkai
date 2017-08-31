3b294158160aa040e798d204434694ffcc36a3f5
dulleh <dulleftovers@gmail.com>
2017-08-31 17:38:20 +0100

    Restructure LibraryUpdateSheet in prep. for delegation

--- app/src/main/kotlin/app/youkai/ui/feature/library_update/LibraryUpdateSheet.kt
+++ app/src/main/kotlin/app/youkai/ui/feature/library_update/LibraryUpdateSheet.kt
@@ -51,24 +51,53 @@ class LibraryUpdateSheet : BottomSheetDialogFragment(), LibraryUpdateView {
         val ARGUMENT_MANGA_ID = "mangaId"
     }
 
-    private val isLollipopOrGreater = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
-
-    private val titleColorLight = R.color.bottom_sheet_title_light
-    private val titleColorDark = R.color.bottom_sheet_title_dark
-    private val labelColorLight = R.color.bottom_sheet_label_light
-    private val labelColorDark = R.color.bottom_sheet_label_dark
-    private val itemColorLight = R.color.bottom_sheet_item_light
-    private val itemColorDark = R.color.bottom_sheet_item_dark
-
     private var mediaType: JsonType? = null
     private var presenter: BaseLibraryUpdatePresenter? = null
     private var statusResolver: StatusResolver? = null
 
+    /**
+     * Lifecycle methods
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setPresenterForArguments()
     }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v: View = inflater.inflate(R.layout.library_update, container, false)
+        setMediaTypeForArguments(v)
+        setViewListeners(v)
+        return v
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        applyLightColors()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        // manually calling presenter method
+        presenter?.detachView(retainInstance = false)
+    }
+
+    override fun onPause() {
+        /**
+         * TODO: change behaviour
+         * Save when:
+         *  - dismissed
+         *  - screen off
+         *  - switched app
+         *  -
+         */
+        presenter?.postUpdate()
+
+        super.onPause()
+    }
+
+    /**
+     * Media type configuration
+     */
     private fun setPresenterForArguments() {
         if (arguments.containsKey(ARGUMENT_LIBRARY_ENTRY_ID)) {
             presenter = BaseLibraryUpdatePresenter()
@@ -93,63 +122,6 @@ class LibraryUpdateSheet : BottomSheetDialogFragment(), LibraryUpdateView {
         } else throw IllegalArgumentException("Cannot create a LibraryUpdateSheet without any relevant arguments.")
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val v: View = inflater.inflate(R.layout.library_update, container, false)
-        setMediaTypeForArguments(v)
-        setViewListeners(v)
-        return v
-    }
-
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        applyLightColors()
-    }
-
-    private fun setViewListeners(rootView: View) {
-        rootView.privacySwitch.setOnCheckedChangeListener { _, isPrivate ->
-            presenter!!.setPrivate(isPrivate)
-            setPrivate(isPrivate)
-        }
-        rootView.statusSpinner.itemSelections()
-                .skipInitialValue()
-                .doOnNext {
-                    if (privacySwitch.isChecked) setSpinnerSelectedColors(itemColorDark, R.color.library_update_button_drawable_dark, R.color.dropdown_dark)
-                    else setSpinnerSelectedColors(itemColorLight, R.color.library_update_button_drawable_light, R.color.dropdown_light)
-                }
-                .filter { rootView.statusSpinner.adapter != null }
-                .map { rootView.statusSpinner.adapter.getItem(it).toString() }
-                .doOnNext { if (statusResolver == null) throw IllegalStateException("Cannot set a status without a statusResolver") }
-                .map { statusResolver!!.getItemStatus(it) }
-                .filter { presenter != null }
-                .doOnNext { presenter!!.setStatus(it) }
-                .subscribe()
-        //TODO: handle rating preferences
-        rootView.ratingBar.setOnRatingChangeListener { _, rating -> presenter!!.setRating(rating) }
-        rootView.notesInputEdit.textChangeEvents()
-                .skipInitialValue()
-                .subscribe { t -> presenter!!.setNotes(t.text().toString()) }
-        rootView.removeButton.clicks().subscribe{ _ -> showRemovalConfirmationDialog() }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        // manually calling presenter method
-        presenter?.detachView(retainInstance = false)
-    }
-
-    override fun onPause() {
-        /**
-         * TODO: change behaviour
-         * Save when:
-         *  - dismissed
-         *  - screen off
-         *  - switched app
-         *  -
-         */
-        presenter?.postUpdate()
-
-        super.onPause()
-    }
 
     private fun mediaTypeIsAnime(mediaType: JsonType? = null): Boolean {
         val type: JsonType = mediaType ?: this.mediaType ?: return false
@@ -176,9 +148,7 @@ class LibraryUpdateSheet : BottomSheetDialogFragment(), LibraryUpdateView {
     /**
      * This exists so that the exposed interface remains simple and without View dependencies.
      */
-    override fun setMediaType(mediaType: JsonType) {
-        setMediaType(mediaType, view)
-    }
+    override fun setMediaType(mediaType: JsonType) = setMediaType(mediaType, view)
 
     private fun setStatusResolverForMediaType(mediaType: JsonType) {
         if (mediaTypeIsAnime()) {
@@ -252,6 +222,35 @@ class LibraryUpdateSheet : BottomSheetDialogFragment(), LibraryUpdateView {
         rootView.statusSpinner.adapter = statusAdapter
     }
 
+    /**
+     * View behaviour and data
+     */
+    private fun setViewListeners(rootView: View) {
+        rootView.privacySwitch.setOnCheckedChangeListener { _, isPrivate ->
+            presenter!!.setPrivate(isPrivate)
+            setPrivate(isPrivate)
+        }
+        rootView.statusSpinner.itemSelections()
+                .skipInitialValue()
+                .doOnNext {
+                    if (privacySwitch.isChecked) setSpinnerSelectedColors(itemColorDark, R.color.library_update_button_drawable_dark, R.color.dropdown_dark)
+                    else setSpinnerSelectedColors(itemColorLight, R.color.library_update_button_drawable_light, R.color.dropdown_light)
+                }
+                .filter { rootView.statusSpinner.adapter != null }
+                .map { rootView.statusSpinner.adapter.getItem(it).toString() }
+                .doOnNext { if (statusResolver == null) throw IllegalStateException("Cannot set a status without a statusResolver") }
+                .map { statusResolver!!.getItemStatus(it) }
+                .filter { presenter != null }
+                .doOnNext { presenter!!.setStatus(it) }
+                .subscribe()
+        //TODO: handle rating preferences
+        rootView.ratingBar.setOnRatingChangeListener { _, rating -> presenter!!.setRating(rating) }
+        rootView.notesInputEdit.textChangeEvents()
+                .skipInitialValue()
+                .subscribe { t -> presenter!!.setNotes(t.text().toString()) }
+        rootView.removeButton.clicks().subscribe{ _ -> showRemovalConfirmationDialog() }
+    }
+
     override fun setTitle(title: String) {
         this.title.text = title
     }
@@ -261,8 +260,101 @@ class LibraryUpdateSheet : BottomSheetDialogFragment(), LibraryUpdateView {
         setPrivateBackground(isPrivate)
     }
 
-    var privacyRippleInset: Float? = null
-    var privacyAnimator: Animator? = null
+
+    private fun showRemovalConfirmationDialog() {
+        val dialog = AlertDialog.Builder(context, R.style.LibraryUpdateDialog)
+                .setTitle(R.string.delete_confirmation_title)
+                .setMessage(R.string.delete_confirmation_message)
+                .setPositiveButton(
+                        R.string.delete_confirmation_positive,
+                        { _, _ ->
+                            presenter!!.removeLibraryEntry()
+                        }
+                )
+                .setNeutralButton(
+                        android.R.string.cancel,
+                        { _, _ ->
+                            //do nothing
+                        }
+                )
+                .create()
+        dialog.show()
+        dialog.getButton(android.app.AlertDialog.BUTTON_NEUTRAL)
+                .setTextColor(getColor(R.color.text_gray_light))
+    }
+
+    override fun setStatus(status: Status) {
+        this.statusSpinner.setSelection(statusResolver!!.getItemPosition(status))
+    }
+
+    override fun setReconsumedCount(reconsumedCount: Int) {
+        reconsumedProgressView.progress = reconsumedCount
+    }
+
+    override fun setRating(rating: Float) {
+        ratingBar.rating = rating
+    }
+
+    override fun setNotes(notes: String) {
+        notesInputEdit.setText(notes)
+    }
+
+    override fun setEpisodeProgress(progress: Int) {
+        if (mediaTypeIsAnime()) {
+            if (progressContainer.findViewById<ProgressView>(R.id.episodesProgressView) != null) episodesProgressView.progress = progress
+            else throw IllegalArgumentException("No episodesProgressView was inflated.")
+        } else throw IllegalArgumentException("Cannot set episode progress for media type: " + mediaType!!.type)
+    }
+
+    override fun setMaxEpisodes(max: Int) {
+        if (mediaTypeIsAnime()) {
+            if (progressContainer.findViewById<ProgressView>(R.id.episodesProgressView) != null) episodesProgressView.max = max
+            else throw IllegalArgumentException("No episodesProgressView was inflated.")
+        } else throw IllegalArgumentException("Cannot set max episodes for media type: " + mediaType!!.type)
+    }
+
+    override fun setChapterProgress(progress: Int) {
+        if (mediaTypeIsManga()) {
+            if (progressContainer.findViewById<ProgressView>(R.id.chaptersProgressView) != null) chaptersProgressView.progress = progress
+            else throw IllegalArgumentException("No chaptersProgressView was inflated.")
+        } else throw IllegalArgumentException("Cannot set chapter progress for media type: " + mediaType!!.type)
+    }
+
+    override fun setMaxChapters(max: Int) {
+        if (mediaTypeIsManga()) {
+            if (progressContainer.findViewById<ProgressView>(R.id.chaptersProgressView) != null) chaptersProgressView.max = max
+            else throw IllegalArgumentException("No chaptersProgressView was inflated.")
+        } else throw IllegalArgumentException("Cannot set max chapters for media type: " + mediaType!!.type)
+    }
+
+    override fun setVolumeProgress(progress: Int) {
+        if (mediaTypeIsManga()) {
+            if (progressContainer.findViewById<ProgressView>(R.id.volumesProgressView) != null) volumesProgressView.progress = progress
+            else throw IllegalArgumentException("No volumesProgressView was inflated.")
+        } else throw IllegalArgumentException("Cannot set volume progress for media type: " + mediaType!!.type)
+    }
+
+    override fun setMaxVolumes(max: Int) {
+        if (mediaTypeIsManga()) {
+            if (progressContainer.findViewById<ProgressView>(R.id.volumesProgressView) != null) volumesProgressView.max = max
+            else throw IllegalArgumentException("No volumesProgressView was inflated.")
+        } else throw IllegalArgumentException("Cannot set max volumes for media type: " + mediaType!!.type)
+    }
+
+    /**
+     * View coloring and animations
+     */
+    private val isLollipopOrGreater = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+
+    private val titleColorLight = R.color.bottom_sheet_title_light
+    private val titleColorDark = R.color.bottom_sheet_title_dark
+    private val labelColorLight = R.color.bottom_sheet_label_light
+    private val labelColorDark = R.color.bottom_sheet_label_dark
+    private val itemColorLight = R.color.bottom_sheet_item_light
+    private val itemColorDark = R.color.bottom_sheet_item_dark
+
+    private var privacyRippleInset: Float? = null
+    private var privacyAnimator: Animator? = null
 
     @SuppressLint("NewApi")
     private fun  setPrivateBackground(isPrivate: Boolean) {
@@ -388,29 +480,26 @@ class LibraryUpdateSheet : BottomSheetDialogFragment(), LibraryUpdateView {
         progressView.findViewById<TextView>(R.id.max)?.setTextColor(textColor)
     }
 
-    private fun retintDrawable(drawable: Drawable, @ColorRes colorStateListRes: Int) {
-        DrawableCompat.setTintList(drawable, resources.getColorStateList(colorStateListRes))
-    }
+    private fun retintDrawable(drawable: Drawable, @ColorRes colorStateListRes: Int)
+            = DrawableCompat.setTintList(drawable, resources.getColorStateList(colorStateListRes))
 
-    private fun applyLightColors() {
-        setColors(
-                titleColorLight,
-                labelColorLight,
-                itemColorLight,
-                R.color.dropdown_light,
-                R.color.library_update_button_drawable_light,
-                R.color.library_update_edittext_light)
-    }
+    private fun applyLightColors() = setColors(
+            titleColorLight,
+            labelColorLight,
+            itemColorLight,
+            R.color.dropdown_light,
+            R.color.library_update_button_drawable_light,
+            R.color.library_update_edittext_light
+    )
 
-    private fun applyDarkColors() {
-        setColors(
-                titleColorDark,
-                labelColorDark,
-                itemColorDark,
-                R.color.dropdown_dark,
-                R.color.library_update_button_drawable_dark,
-                R.color.library_update_edittext_dark)
-    }
+    private fun applyDarkColors() = setColors(
+            titleColorDark,
+            labelColorDark,
+            itemColorDark,
+            R.color.dropdown_dark,
+            R.color.library_update_button_drawable_dark,
+            R.color.library_update_edittext_dark
+    )
 
     private fun getColor(@ColorRes colorRes: Int): Int {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -420,89 +509,11 @@ class LibraryUpdateSheet : BottomSheetDialogFragment(), LibraryUpdateView {
         }
     }
 
-    private fun showRemovalConfirmationDialog() {
-        val dialog = AlertDialog.Builder(context, R.style.LibraryUpdateDialog)
-                .setTitle(R.string.delete_confirmation_title)
-                .setMessage(R.string.delete_confirmation_message)
-                .setPositiveButton(
-                        R.string.delete_confirmation_positive,
-                        { _, _ ->
-                            presenter!!.removeLibraryEntry()
-                        }
-                )
-                .setNeutralButton(
-                        android.R.string.cancel,
-                        { _, _ ->
-                            //do nothing
-                        }
-                )
-                .create()
-        dialog.show()
-        dialog.getButton(android.app.AlertDialog.BUTTON_NEUTRAL)
-                .setTextColor(getColor(R.color.text_gray_light))
-    }
-
-    override fun setStatus(status: Status) {
-        this.statusSpinner.setSelection(statusResolver!!.getItemPosition(status))
-    }
-
-    override fun setReconsumedCount(reconsumedCount: Int) {
-        reconsumedProgressView.progress = reconsumedCount
-    }
-
-    override fun setRating(rating: Float) {
-        ratingBar.rating = rating
-    }
-
-    override fun setNotes(notes: String) {
-        notesInputEdit.setText(notes)
-    }
-
-    override fun setEpisodeProgress(progress: Int) {
-        if (mediaTypeIsAnime()) {
-            if (progressContainer.findViewById<ProgressView>(R.id.episodesProgressView) != null) episodesProgressView.progress = progress
-            else throw IllegalArgumentException("No episodesProgressView was inflated.")
-        } else throw IllegalArgumentException("Cannot set episode progress for media type: " + mediaType!!.type)
-    }
-
-    override fun setMaxEpisodes(max: Int) {
-        if (mediaTypeIsAnime()) {
-            if (progressContainer.findViewById<ProgressView>(R.id.episodesProgressView) != null) episodesProgressView.max = max
-            else throw IllegalArgumentException("No episodesProgressView was inflated.")
-        } else throw IllegalArgumentException("Cannot set max episodes for media type: " + mediaType!!.type)
-    }
-
-    override fun setChapterProgress(progress: Int) {
-        if (mediaTypeIsManga()) {
-            if (progressContainer.findViewById<ProgressView>(R.id.chaptersProgressView) != null) chaptersProgressView.progress = progress
-            else throw IllegalArgumentException("No chaptersProgressView was inflated.")
-        } else throw IllegalArgumentException("Cannot set chapter progress for media type: " + mediaType!!.type)
-    }
-
-    override fun setMaxChapters(max: Int) {
-        if (mediaTypeIsManga()) {
-            if (progressContainer.findViewById<ProgressView>(R.id.chaptersProgressView) != null) chaptersProgressView.max = max
-            else throw IllegalArgumentException("No chaptersProgressView was inflated.")
-        } else throw IllegalArgumentException("Cannot set max chapters for media type: " + mediaType!!.type)
-    }
-
-    override fun setVolumeProgress(progress: Int) {
-        if (mediaTypeIsManga()) {
-            if (progressContainer.findViewById<ProgressView>(R.id.volumesProgressView) != null) volumesProgressView.progress = progress
-            else throw IllegalArgumentException("No volumesProgressView was inflated.")
-        } else throw IllegalArgumentException("Cannot set volume progress for media type: " + mediaType!!.type)
-    }
-
-    override fun setMaxVolumes(max: Int) {
-        if (mediaTypeIsManga()) {
-            if (progressContainer.findViewById<ProgressView>(R.id.volumesProgressView) != null) volumesProgressView.max = max
-            else throw IllegalArgumentException("No volumesProgressView was inflated.")
-        } else throw IllegalArgumentException("Cannot set max volumes for media type: " + mediaType!!.type)
-    }
-
-    override fun sendToLogin() {
-        startActivityForResult(Intent(context, LoginActivity::class.java), LoginActivity.START_FOR_ACCESS_TOKEN)
-    }
+    /**
+     * Login boilerplate
+     */
+    override fun sendToLogin()
+            = startActivityForResult(Intent(context, LoginActivity::class.java), LoginActivity.START_FOR_ACCESS_TOKEN)
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
