e606fd400e30788e01060c07245ac080d8c4c631
Charles Madere <charlesrmadere@gmail.com>
2017-09-12 20:41:40 -0700

    you can now logout

--- app/src/main/kotlin/app/youkai/ui/feature/settings/SettingsActivity.kt
+++ app/src/main/kotlin/app/youkai/ui/feature/settings/SettingsActivity.kt
@@ -3,7 +3,9 @@ package app.youkai.ui.feature.settings
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import app.youkai.BuildConfig
+import app.youkai.MainActivity
 import app.youkai.R
 import app.youkai.data.local.Credentials
 import com.hannesdorfmann.mosby.mvp.viewstate.MvpViewStateActivity
@@ -14,6 +16,19 @@ class SettingsActivity : MvpViewStateActivity<SettingsView, SettingsPresenter>()
     override fun createPresenter() = SettingsPresenter()
 
     override fun createViewState() = SettingsState()
+
+    private fun logout() {
+        AlertDialog.Builder(this)
+                .setMessage(R.string.logout_confirmation)
+                .setNegativeButton(R.string.cancel, null)
+                .setPositiveButton(R.string.ok, { dialog, which ->
+                    Credentials.logout()
+                    startActivity(Intent.makeRestartActivityTask(
+                            MainActivity.getLaunchIntent(this).component))
+                    finish()
+                })
+                .show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +46,7 @@ class SettingsActivity : MvpViewStateActivity<SettingsView, SettingsPresenter>()
 
         logout.description = getString(R.string.you_are_logged_in_as_x, Credentials.username)
         logout.setOnClickListener {
-
+            logout()
         }
 
         versionInfo.description = resources.getString(R.string.version_information_format,
